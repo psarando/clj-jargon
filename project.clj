0e06b8fc0cae574513a8370f33f435143dc2f1c1
Ian McEwen <mian@cyverse.org>
2016-09-21 11:44:57 -0700

    Add test2junit, publish test results, and remove AbortException case in Jenkinsfile

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
+  :plugins [[test2junit "1.2.2"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
